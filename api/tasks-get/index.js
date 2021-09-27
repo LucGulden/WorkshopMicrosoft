0e9e18c6f23371d2db317680dc590713fbedcc4e
LucGulden <luc.gulden@viacesi.fr>
2021-09-27 16:54:54 +0200

    test

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -18,7 +18,7 @@ module.exports = async function (context, req) {
             status: ""
         }
     ];
-
+    
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -26,6 +26,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: tasks
+        body: responseMessage
     };
 }
